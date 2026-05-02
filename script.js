a4694712feac1e60b25a66f17b77037cbf1d88aa
Sharon Wambui <sharonwambui669@gmail.com>
2026-05-02 16:20:59 +0300

    Created function to assign Akan name based on gender

--- script.js
+++ script.js
@@ -8,7 +8,25 @@ const days=["Sunday","Monday","Tuesday","Wednesday",
         const femaleNames=["Akwosua","Adwoa","Abenaa",
             "Akua","Yaa","Afua","Ama"];
 
-            
+            //function to get day of the week
+            function getDayofWeek(day,month,year) {
+                const date=new Date(year,month -1,day);
+                return date.getDate();//returns 0-6
+            }
+
+            //Function to get Akan name
+            function getAkanName(dayIndex,gender){
+                if(gender ==="male") {
+                    //Your logic for male names goes here
+                    }else{
+                        //Your logic for female names goes here
+                    }
+                 }
+
+                 //Function to validate input
+                 
+
+
 
 
     
