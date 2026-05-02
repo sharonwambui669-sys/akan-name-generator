4dc282f5d50d7cf5114e7e4a006cdbe88d31e986
Sharon Wambui <sharonwambui669@gmail.com>
2026-05-02 16:04:48 +0300

    added arrays for days and Akan names

---
+++ script.js
@@ -0,0 +1,14 @@
+//Arrays for days and Akan names
+const days=["Sunday","Monday","Tuesday","Wednesday",
+    "Thursday","Friday","Saturday","Sunday"];
+
+    const maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw",
+        "Kofi","Kwame"];
+
+        const femaleNames=["Akwosua","Adwoa","Abenaa",
+            "Akua","Yaa","Afua","Ama"];
+
+            
+
+
+    
